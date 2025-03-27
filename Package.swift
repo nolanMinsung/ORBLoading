2118204db2f5b543f63df036966e0be431b193eb
Minsung Kim <mskim4048@naver.com>
2025-03-27 22:41:21 +0900

    add Library dependencies "lottie-ios"

--- Package.swift
+++ Package.swift
@@ -11,6 +11,9 @@ let package = Package(
             name: "ORBLoading",
             targets: ["ORBLoading"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/airbnb/lottie-ios.git", from: .init(4, 5, 1))
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
