c2a68ef6af9499b2b5f0b063599548540681fd9d
Minsung Kim <mskim4048@naver.com>
2025-03-29 01:44:59 +0900

    Add ORBLoadingStyle, ORBScrollLoadingStyle

--- Sources/ORBLoading/ORBLoading.swift
+++ Sources/ORBLoading/ORBLoading.swift
@@ -1,2 +1,60 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+
+import UIKit
+
+import Lottie
+import SnapKit
+
+protocol ORBLoadingStyle: UIView {
+    
+    func startLoading(withoutShading: Bool)
+    func stopLoading()
+    
+}
+
+extension ORBLoadingStyle {
+    
+    func startLoading(withoutShading: Bool) {
+        let loadingView = ORBLoadingView()
+        if withoutShading { loadingView.backgroundColor = .black.withAlphaComponent(0.55) }
+        
+        addSubview(loadingView)
+        
+        if let scrollView = self as? UIScrollView {
+            loadingView.snp.makeConstraints { $0.edges.equalTo(scrollView.frameLayoutGuide) }
+        } else {
+            loadingView.snp.makeConstraints { $0.edges.equalToSuperview() }
+        }
+    }
+    
+    func stopLoading() {
+        subviews.forEach { view in
+            if let loadingView = view as? ORBLoadingView { loadingView.removeFromSuperview() }
+        }
+    }
+    
+}
+
+final class ORBLoadingView: UIView {
+    
+    let lottie = Lottie.LottieAnimationView(name: "loading2")
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        lottie.loopMode = .loop
+        lottie.play()
+        addSubview(lottie)
+        lottie.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.size.equalTo(150)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
---
+++ Sources/ORBLoading/ORBScrollLoading.swift
@@ -0,0 +1,82 @@
+//
+//  File.swift
+//  ORBLoading
+//
+//  Created by 김민성 on 3/28/25.
+//
+
+import UIKit
+
+import Lottie
+import SnapKit
+
+
+/// 무한스크롤 시 scroll view 컨텐츠 아래에 로딩 화면을 띄우고자 할 경우, 해당 scroll view에서 이 프로토콜을 채택
+protocol ORBScrollLoadingStyle: UIScrollView {
+    
+    var scrollLoadingLottieOffset: CGFloat { get }
+    var scrollLoadingLottieSideLength: CGFloat { get }
+    var additionalContentInset: CGFloat { get }
+    
+    func startBottomScrollLoading()
+    func stopBottomScrollLoading()
+    
+}
+
+extension ORBScrollLoadingStyle {
+    
+    var scrollLoadingLottieOffset: CGFloat { 18 }
+    var scrollLoadingLottieSideLength: CGFloat { 38 }
+    var additionalContentInset: CGFloat { scrollLoadingLottieSideLength + (scrollLoadingLottieOffset * 2) }
+    
+    /// scroll view 아래쪽의 무한 스크롤 로딩 화면을 띄웁니다.
+    func startBottomScrollLoading() {
+        let scrollLoadingView = ORBScrollLoadingView()
+        contentInset.bottom += additionalContentInset
+        addSubview(scrollLoadingView)
+        layoutIfNeeded()
+        scrollLoadingView.frame = .init(
+            x: bounds.width/2 - scrollLoadingLottieSideLength/2,
+            y: contentSize.height + scrollLoadingLottieOffset,
+            width: scrollLoadingLottieSideLength,
+            height: scrollLoadingLottieSideLength
+        )
+    }
+    
+    /// scroll view 컨텐츠 아래쪽에 무한 스크롤 로딩이 있다면 로딩을 멈춥니다.
+    func stopBottomScrollLoading() {
+        var isScrollLoading: Bool = false
+        subviews.forEach { view in
+            if let scrollLoadingView = view as? ORBScrollLoadingView {
+                scrollLoadingView.removeFromSuperview()
+                isScrollLoading = true
+            }
+        }
+        if isScrollLoading {
+            contentInset.bottom -= additionalContentInset
+        }
+    }
+    
+}
+
+final class ORBScrollLoadingView: UIView {
+    
+    let lottie = Lottie.LottieAnimationView(name: "loading1")
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        isUserInteractionEnabled = false
+        lottie.loopMode = .loop
+        lottie.play()
+        addSubview(lottie)
+        lottie.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
